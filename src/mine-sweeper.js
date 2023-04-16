e22975a0d4b240f6ca97325414cefc7f4bf4133d
shep-js <kateshakurina@gmail.com>
2023-04-16 19:19:02 +0200

    feat: add mine sweeper solution

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,11 +23,49 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(arr) {
+  let ans = [];
+  while (ans.length != arr.length) {
+    ans.push([]);
+  }
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      if (arr[i - 1] != undefined) {
+        if (arr[i - 1][j - 1] != undefined && arr[i - 1][j - 1] === true) {
+          count++;
+        }
+        if (arr[i - 1][j] != undefined && arr[i - 1][j] === true) {
+          count++;
+        }
+        if (arr[i - 1][j + 1] != undefined && arr[i - 1][j + 1] === true) {
+          count++;
+        }
+      }
+      if (arr[i][j - 1] != undefined && arr[i][j - 1] === true) {
+        count++;
+      }
+      if (arr[i][j + 1] != undefined && arr[i][j + 1] === true) {
+        count++;
+      }
+      if (arr[i + 1] != undefined) {
+        if (arr[i + 1][j - 1] != undefined && arr[i + 1][j - 1] === true) {
+          count++;
+        }
+        if (arr[i + 1][j] != undefined && arr[i + 1][j] === true) {
+          count++;
+        }
+        if (arr[i + 1][j + 1] != undefined && arr[i + 1][j + 1] === true) {
+          count++;
+        }
+      }
+      ans[i].push(count);
+      count = 0;
+    }
+  }
+  return ans;
 }
 
 module.exports = {
-  minesweeper
+  minesweeper,
 };
